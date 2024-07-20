f42888f82529b7a6b21b272c4130b0e57209bf06
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-07-20 00:53:56 -0700

    new version 0.2

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,19 @@
-from django.urls import path
+from django.urls import path, register_converter, re_path
 from blog.views import post_list, categories_list, post_detail, custom_post_detail
+from blog.utils import FourDigitYear
+
+
+register_converter(FourDigitYear, "fourdigit")
 
 urlpatterns = [
-    path("list/", post_list),
-    # path('detail/<int:post_id>/',post_detail),
-    # path('detail/<uuid:post_uuid>/',post_detail),
-    path("detail/hello/", custom_post_detail),
-    path("detail/<slug:post_slug>/", post_detail),
-    path("categories/list/", categories_list),
-    path("archive/<int:year>/", post_list),
-    path("archive/<int:year>/<int:month>/", post_list),
+    path("list/", post_list, name="post_list"),
+    path("detail/hello/", custom_post_detail, name="custom_post_detail"),
+    path("detail/<slug:post_slug>/", post_detail, name="post_detail"),
+    path("categories/list/", categories_list, name="categories_list"),
+    path("archive/<fourdigit:year>/", post_list, name="archive_year"),
+    path("archive/<int:year>/<int:month>/", post_list, name="archive_year_month"),
+    
+    re_path(r"^detail/(?P<post_slug>[\w-]+)/$", post_detail, name="post_detail_re"),
+    re_path(r"^archive/(?P<year>[0-9]{2,4})/$", post_list, name="archive_year_re"),
+    re_path(r"^archive/(?P<code>[0-9]{4,6})/$", post_list, name="archive_code"),
 ]
