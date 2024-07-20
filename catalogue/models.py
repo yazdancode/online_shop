594d2a59dc6f48fa4ee6ddf6d2a23bbbf94992cd
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-07-20 05:50:30 -0700

    new version 0.3

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,3 +1,113 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductType(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Product Type"
+        verbose_name_plural = "Product Types"
+
+    def __str__(self):
+        return self.title
+
+
+class ProductAttribute(models.Model):
+    INTEGER = 1
+    STRING = 2
+    FLOAT = 3
+
+    ATTRIBUTE_TYPES_FIELDS = (
+        (INTEGER, "Integer"),
+        (STRING, "String"),
+        (FLOAT, "Float"),
+    )
+    title = models.CharField(max_length=255)
+    product_type = models.ForeignKey(
+        ProductType, on_delete=models.CASCADE, related_name="attributes"
+    )
+    attribute_type = models.PositiveSmallIntegerField(
+        default=INTEGER, choices=ATTRIBUTE_TYPES_FIELDS
+    )
+
+    class Meta:
+        verbose_name = "Product Attribute"
+        verbose_name_plural = "Product Attributes"
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="subcategories",
+    )
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="sub_brands",
+    )
+
+    class Meta:
+        verbose_name = "Brand"
+        verbose_name_plural = "Brands"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    product_type = models.ForeignKey(ProductType, on_delete=models.PROTECT)
+    upc = models.BigIntegerField(unique=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(
+        Category, related_name="category_products", on_delete=models.CASCADE
+    )
+    brand = models.ForeignKey(Brand, related_name="products", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.title
+
+
+class ProductAttributeValue(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="attribute_values"
+    )
+    value = models.CharField(max_length=48)
+    attribute = models.ForeignKey(
+        ProductAttribute, on_delete=models.PROTECT, related_name="values"
+    )
+
+    class Meta:
+        verbose_name = "Product Attribute Value"
+        verbose_name_plural = "Product Attribute Values"
+
+    def __str__(self):
+        return f"{self.product}({self.attribute}): {self.value}"
