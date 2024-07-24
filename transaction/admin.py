c5ac2b4cd24c6be471c7b2186a9c45926bc9242b
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-07-24 01:00:30 -0700

    new version 0.5

--- transaction/admin.py
+++ transaction/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-# Register your models here.
+from transaction.models import Transaction, UserBalance
+
+
+@register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ["user", "transaction_type", "amount", "created_time"]
+    list_filter = ["transaction_type"]
+    search_fields = ["user__username"]
+
+
+@register(UserBalance)
+class UserBalanceAdmin(admin.ModelAdmin):
+    list_display = ["user", "balance", "created_time"]
+    search_fields = ["user__username"]
--- transaction/models.py
+++ transaction/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Count, Sum, Q
+from django.db.models.functions import Coalesce
 
 
 class Transaction(models.Model):
@@ -12,6 +14,7 @@ class Transaction(models.Model):
         (PURCHASE, "Purchase"),
         (TRANSFER, "Transfer"),
     )
+
     user = models.ForeignKey(
         User, related_name="transactions", on_delete=models.RESTRICT
     )
@@ -22,4 +25,37 @@ class Transaction(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user}- {self.get_transaction_type_display()}- {self.amount}"
+        return f"{self.user} - {self.get_transaction_type_display()} - {self.amount}"
+
+    @classmethod
+    def get_report(cls):
+        """Show all users and their balance"""
+        positive_transactions = Sum(
+            "transactions__amount", filter=Q(transactions__transaction_type=cls.CHARGE)
+        )
+        negative_transactions = Sum(
+            "transactions__amount",
+            filter=Q(transactions__transaction_type__in=[cls.PURCHASE, cls.TRANSFER]),
+        )
+        users = User.objects.all().annotate(
+            transaction_count=Count("transactions__id"),
+            balance=Coalesce(positive_transactions, 0)
+            - Coalesce(negative_transactions, 0),
+        )
+        return users
+
+    @classmethod
+    def get_total_balance(cls):
+        queryset = cls.get_report()
+        return queryset.aggregate(Sum("balance"))
+
+
+class UserBalance(models.Model):
+    user = models.ForeignKey(
+        User, related_name="balance_records", on_delete=models.PROTECT
+    )
+    balance = models.BigIntegerField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.balance} - {self.created_time}"
