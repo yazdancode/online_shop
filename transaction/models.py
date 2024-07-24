b6f6d44b3af5407fcdd5dabc6916e39f4f85368b
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-07-24 02:50:34 -0700

    new version 05

--- transaction/models.py
+++ transaction/models.py
@@ -7,19 +7,21 @@ from django.db.models.functions import Coalesce
 class Transaction(models.Model):
     CHARGE = 1
     PURCHASE = 2
-    TRANSFER = 3
+    TRANSFER_RECEIVED = 3
+    TRANSFER_SENT = 4
 
-    Transaction_TYPE_CHOICES = (
+    TRANSACTION_TYPE_CHOICES = (
         (CHARGE, "Charge"),
         (PURCHASE, "Purchase"),
-        (TRANSFER, "Transfer"),
+        (TRANSFER_SENT, "Transfer sent"),
+        (TRANSFER_RECEIVED, "Transfer received"),
     )
 
     user = models.ForeignKey(
         User, related_name="transactions", on_delete=models.RESTRICT
     )
     transaction_type = models.PositiveSmallIntegerField(
-        choices=Transaction_TYPE_CHOICES, default=CHARGE
+        choices=TRANSACTION_TYPE_CHOICES, default=CHARGE
     )
     amount = models.BigIntegerField()
     created_time = models.DateTimeField(auto_now_add=True)
@@ -31,11 +33,16 @@ class Transaction(models.Model):
     def get_report(cls):
         """Show all users and their balance"""
         positive_transactions = Sum(
-            "transactions__amount", filter=Q(transactions__transaction_type=cls.CHARGE)
+            "transactions__amount",
+            filter=Q(
+                transactions__transaction_type__in=[cls.CHARGE, cls.TRANSFER_RECEIVED]
+            ),
         )
         negative_transactions = Sum(
             "transactions__amount",
-            filter=Q(transactions__transaction_type__in=[cls.PURCHASE, cls.TRANSFER]),
+            filter=Q(
+                transactions__transaction_type__in=[cls.PURCHASE, cls.TRANSFER_SENT]
+            ),
         )
         users = User.objects.all().annotate(
             transaction_count=Count("transactions__id"),
@@ -49,6 +56,21 @@ class Transaction(models.Model):
         queryset = cls.get_report()
         return queryset.aggregate(Sum("balance"))
 
+    @classmethod
+    def user_balance(cls, user):
+        positive_transactions = Sum(
+            "amount", filter=Q(transaction_type__in=[cls.CHARGE, cls.TRANSFER_RECEIVED])
+        )
+        negative_transactions = Sum(
+            "amount", filter=Q(transaction_type__in=[cls.PURCHASE, cls.TRANSFER_SENT])
+        )
+
+        user_balance = user.transactions.all().aggregate(
+            balance=Coalesce(positive_transactions, 0)
+            - Coalesce(negative_transactions, 0)
+        )
+        return user_balance.get("balance", 0)
+
 
 class UserBalance(models.Model):
     user = models.ForeignKey(
@@ -59,3 +81,42 @@ class UserBalance(models.Model):
 
     def __str__(self):
         return f"{self.user} - {self.balance} - {self.created_time}"
+
+    @classmethod
+    def record_user_balance(cls, user):
+        balance = Transaction.user_balance(user=user)
+        instance = cls.objects.create(user=user, balance=balance)
+        return instance
+
+    @classmethod
+    def record_all_users_balance(cls):
+        for user in User.objects.all():
+            cls.record_user_balance(user)
+
+
+class TransferTransactions(models.Model):
+    sender_transaction = models.ForeignKey(
+        Transaction, related_name="sent_transfers", on_delete=models.RESTRICT
+    )
+    receiver_transaction = models.ForeignKey(
+        Transaction, related_name="received_transfers", on_delete=models.RESTRICT
+    )
+
+    def __str__(self):
+        return f"{self.sender_transaction} >> {self.receiver_transaction}"
+
+    @classmethod
+    def transfer(cls, sender, receiver, amount):
+        if Transaction.user_balance(sender) < amount:
+            return "Transaction not allowed, insufficient balance"
+        sender_transaction = Transaction.objects.create(
+            user=sender, amount=amount, transaction_type=Transaction.TRANSFER_SENT
+        )
+        receiver_transaction = Transaction.objects.create(
+            user=receiver, amount=amount, transaction_type=Transaction.TRANSFER_RECEIVED
+        )
+        instance = cls.objects.create(
+            sender_transaction=sender_transaction,
+            receiver_transaction=receiver_transaction,
+        )
+        return instance
