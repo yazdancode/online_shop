b433bc1932928d7368ecfd8343e63ad52cb39ff4
yshabanei <168936995+yshabanei@users.noreply.github.com>
2024-07-27 22:57:29 -0700

    new version 0.6

--- catalogue/views.py
+++ catalogue/views.py
@@ -36,18 +36,16 @@ def category_products(request, pk):
 
 def product_search(request):
     title = request.GET.get("q")
-    products = Product.objects.filter(
-        is_active=True,
+    products = Product.objects.actives(
         title__icontains=title,
         category__name__icontains=title,
-        category__is_active=True,
     )
-    products = (
-        Product.objects.filter(is_active=True)
-        .filter(title__icontains=title)
-        .filter(category__name__icontains=title)
-        .distinct()
-        .filter(category__is_active=True)
+    # products = (
+    #     Product.objects.filter(is_active=True)
+    #     .filter(title__icontains=title)
+    #     .filter(category__name__icontains=title)
+    #     .distinct()
+    #     .filter(category__is_active=True)
     )
     context = "\n".join([f"{product.title}, {product.upc}" for product in products])
     return HttpResponse(f"Search pages:\n{context}")
